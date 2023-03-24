7a52745189344ea97119ba67c8cabe35e1121f8a
anurag-ux <anurag.s@refactor.academy>
2023-03-24 11:25:55 +0530

    converted files to jsx

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Introduction from "../components/Introduction";
+import Navbar from "../components/Navbar";
+import Projects from "./Projects"
+import MoveToTopButton from "../components/Movetotop"
+
+import './stylesheets/Landing.css';
+
+function Landing() {
+    return (
+        <div className="landing">
+            <Navbar />
+            <Introduction />
+            <MoveToTopButton/>
+            <Projects />
+        </div>
+    );
+}
+
+export default Landing;
