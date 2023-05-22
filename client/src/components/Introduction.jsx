8eb9e7267243a998629d67a5eb8e571c0e19000a
Anurag Shandilya <anurag-shandilya@anushan-mbp.optilink>
2023-05-23 05:15:51 +0530

    fixed responsive css of introduction

--- client/src/components/Introduction.jsx
+++ client/src/components/Introduction.jsx
@@ -4,25 +4,20 @@ import './stylesheets/Introduction.css'
 
 function Introduction() {
     return (
-        <div id='intro' className='main-introduction'>
-            <img alt='main' className='main-picture'
-                src='https://gistcdn.githack.com/anurag-ux/a5340bb04294e23431da2c806f25ccb4/raw/e46ad68648e00f57707d51c9ce5f0417cb15b6bb/main-picture.svg'>
-            </img>
-            <div className='introduction'>
-                <p className='intro'>Hello! I am<span className='keyword' style={{'paddingLeft': '1%'}}>Anurag Shandilya</span>
-                <Footer/>
-                </p>
+        <div className='top-div'>
+            <div id='intro' className='main-introduction'>
+                <img alt='main' className='main-picture'
+                    src='https://gistcdn.githack.com/anurag-ux/a5340bb04294e23431da2c806f25ccb4/raw/e46ad68648e00f57707d51c9ce5f0417cb15b6bb/main-picture.svg'>
+                </img>
+                <div className='introduction'>
+                    <p className='intro'>I'm<span className='keyword'> Anurag Shandilya</span>
+                        <Footer />
+                    </p>
+                </div>
             </div>
             <div className='experience'>
-                <p className='roles'>
-                    I'm a Product Manager
-                </p>
-                <p className='current-role'>
-                    Currently, I work as a Product Owner at <img alt='logo' className='refactor-logo'
-                        src='https://gist.github.com/anurag-ux/eabbdac24254842fe4e8b1bca6410598/raw/3beaca2f5fad7398d07759865296a1e1da0668c4/logo.svg'/>
-                </p>
                 <p className='experience-description'>
-                    A self-taught Software Developer & a PM, functioning in the industry for 1+ years now.
+                    I'm a self-taught Software Developer & a Product Manager, functioning in the industry for 1+ years now.
                     I make meaningful and delightful digital products that create an equilibrium between user needs and business goals.
                 </p>
             </div>
