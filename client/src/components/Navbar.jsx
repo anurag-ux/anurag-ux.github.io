13a547c34e478f7dfacb72f255ff41de49877045
Anurag Shandilya <anurag-shandilya@anushan-mbp.optilink>
2023-05-23 04:23:29 +0530

    fixed navbar css, made responsive

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,35 +7,30 @@ function Navbar() {
         event.preventDefault();
         const projects = document.getElementById("experience");
         projects.scrollIntoView({ behavior: "smooth", block: "start" });
-      };
+    };
     const handleClickProjects = (event) => {
         event.preventDefault();
         const projects = document.getElementById("projects");
         projects.scrollIntoView({ behavior: "smooth", block: "start" });
-      };
-      const handleClickHome = (event) => {
-        event.preventDefault();
-        const projects = document.getElementById("intro");
-        projects.scrollIntoView({ behavior: "smooth", block: "start" });
-      };
+    };
     return (
         <div className="navigation">
             <nav>
                 <ul className="navbar-list">
-                    <li className="navbar-item">
-                        <a href="/" className="navbar-link" onClick={handleClickHome}>Home</a>
-                    </li>
                     <li className="navbar-item">
                         <a href="/" className="navbar-link" onClick={handleClickExperience}>Experience</a>
                     </li>
                     <li className="navbar-item">
                         <a href="/" className="navbar-link" onClick={handleClickProjects}>Projects</a>
                     </li>
+                    <li className="navbar-item">
+                        <a rel="noreferrer" className="navbar-link" href="https://shorturl.at/benOU" target="_blank">
+                            Resume
+                        </a>
+                    </li>
                 </ul>
-                <a rel="noreferrer" className="download-resume" href="https://shorturl.at/benOU" target="_blank">
-                    Download Resume</a>
             </nav>
-            <hr className="header-divider"/>
+            <hr className="header-divider" />
         </div>
     );
 }
